bdb187ec4d8ab459c442e1cbf9c8b2faf46731f1
Klapaucius <39060010+CornelTutui@users.noreply.github.com>
2021-05-29 21:20:17 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+/// <reference path="../node_modules/@types/leaflet/index.d.ts" />
+var LeafletMap = /** @class */ (function () {
+    function LeafletMap(p) {
+        var map = new L.Map(p);
+        //map.setView(new L.LatLng(44.52787, 25.96771), 16);
+        map.locate({ setView: true, maxZoom: 18, enableHighAccuracy: true, maximumAge: 10000 });
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1
+        }).addTo(map);
+        map.on('locationfound', onLocationFound);
+        map.on('locationerror', onLocationError);
+        function onLocationFound(e) {
+            var radius = e.accuracy;
+            L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
+            L.circle(e.latlng, radius).addTo(map);
+        }
+        function onLocationError(e) {
+            alert(e.message);
+        }
+    }
+    return LeafletMap;
+}());
+window.onload = function () {
+    var p = document.getElementById("content");
+    var map = new LeafletMap(p);
+};
+//# sourceMappingURL=app.js.map
