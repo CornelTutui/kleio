f37f17c650910767b220b918791770e45038e219
Klapaucius <39060010+CornelTutui@users.noreply.github.com>
2021-05-29 22:20:20 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,9 @@
 var LeafletMap = /** @class */ (function () {
     function LeafletMap(p) {
         var map = new L.Map(p);
-        //map.setView(new L.LatLng(44.52787, 25.96771), 16);
-        map.locate({ setView: true, maxZoom: 18, enableHighAccuracy: true, maximumAge: 10000 });
+        map.locate({ watch: true, setView: true, /*maxZoom: 16,*/ enableHighAccuracy: true });
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-            maxZoom: 18,
+            /*maxZoom: 18,*/
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
                 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             id: 'mapbox/streets-v11',
@@ -16,7 +15,7 @@ var LeafletMap = /** @class */ (function () {
         map.on('locationerror', onLocationError);
         function onLocationFound(e) {
             var radius = e.accuracy;
-            L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
+            //L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
             L.circle(e.latlng, radius).addTo(map);
         }
         function onLocationError(e) {
