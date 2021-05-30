2606a78cd8771b856f4edb97f4bb0bfed8b58d05
Klapaucius <39060010+CornelTutui@users.noreply.github.com>
2021-05-30 17:24:08 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var LeafletMap = /** @class */ (function () {
     function LeafletMap(p) {
         var map = new L.Map(p);
-        map.locate({ watch: true, setView: true, maxZoom: 18, enableHighAccuracy: true });
+        map.locate({ /*watch: true, */ setView: true, maxZoom: 18, enableHighAccuracy: true });
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
             maxZoom: 18,
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
